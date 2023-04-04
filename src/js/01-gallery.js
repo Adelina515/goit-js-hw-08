6f224610bb3eff29238d3f7d6dfdfcc814f88c44
Adelina515 <adelgodlevska@gmail.com>
2023-04-04 23:58:30 +0300

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryUl = document.querySelector(".gallery");
+const galleryMarkupLi = galleryItems.map(({ preview, original, description }) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>
+    </li>`).join(" ");
+    galleryUl.insertAdjacentHTML("afterbegin", galleryMarkupLi);
+galleryUl.addEventListener('click', handleModalOpen);
+
+function handleModalOpen(event) {
+    event.preventDefault();
+    const currentLi = event.target;
+
+     if (currentLi.nodeName !== "IMG") {
+    return;
+     }
+    const lightbox = new SimpleLightbox('.gallery .gallery__link', {
+        captions: true,
+        captionsData: "alt",
+        captionPosition: "bottom",
+        captionDelay: 250,
+});
+  
+}; 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import player from "@vimeo/player";
+   import throttle from "lodash.throttle";
 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('timeupdate', _.throttle(onPlay, 1000))
+        
+   localStorage.setItem("videoplayer-current-time", "videoplayer-current-time");
+    
+    player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+    });
+    /*
+    Вивчи документацію методу on() і почни відстежувати подію timeupdate - 
+    оновлення часу відтворення.
+Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде
+ рядок "videoplayer-current-time".
+Під час перезавантаження сторінки скористайся методом setCurrentTime() з 
+метою відновлення відтворення зі збереженої позиції.
+Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час 
+відтворення оновлювався у сховищі не частіше, ніж раз на секунду. */
