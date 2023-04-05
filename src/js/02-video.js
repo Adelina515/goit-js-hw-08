950239905ccd60a0c6dfd9054905537041ab0754
Adelina515 <adelgodlevska@gmail.com>
2023-04-05 16:21:40 +0300

    add instal

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
-// import player from "@vimeo/player";
-//    import throttle from "lodash.throttle";
+import player from "@vimeo/player";
+   import throttle from "lodash.throttle";
 
-//     const iframe = document.querySelector('iframe');
-//     const player = new Vimeo.Player(iframe);
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
 //     player.on('timeupdate', _.throttle(onPlay, 1000));
         
 //    localStorage.setItem("videoplayer-current-time", "videoplayer-current-time");
