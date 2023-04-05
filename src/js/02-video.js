3b3f8f481cbfc718ce472da809420db0a2bd6711
Adelina515 <adelgodlevska@gmail.com>
2023-04-05 15:28:56 +0300

    add coment

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,11 @@
-import player from "@vimeo/player";
-   import throttle from "lodash.throttle";
+// import player from "@vimeo/player";
+//    import throttle from "lodash.throttle";
 
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
-    player.on('timeupdate', _.throttle(onPlay, 1000));
+//     const iframe = document.querySelector('iframe');
+//     const player = new Vimeo.Player(iframe);
+//     player.on('timeupdate', _.throttle(onPlay, 1000));
         
-   localStorage.setItem("videoplayer-current-time", "videoplayer-current-time");
+//    localStorage.setItem("videoplayer-current-time", "videoplayer-current-time");
     
     /*
     Вивчи документацію методу on() і почни відстежувати подію timeupdate - 
