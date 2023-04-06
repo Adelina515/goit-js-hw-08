936ec21dcaa0ac4bc8f20ef31831bc34532110d4
Adelina515 <adelgodlevska@gmail.com>
2023-04-06 18:05:51 +0300

    add task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,30 @@
-// import player from "@vimeo/player";
-//    import throttle from "lodash.throttle";
-    import player from "@vimeo/player";
+
+    import Player from "@vimeo/player";
     import throttle from "lodash.throttle";
     
     const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
-     player.on('timeupdate', function(data) {
-         const currentTime = data.currentTarget;
-         console.log(data.currentTarget);
-    });
-        
-//    localStorage.setItem("videoplayer-current-time", "videoplayer-current-time");
+    
+    const LOCALCURRENTTIME_KEY = "videoplayer-current-time";
+    
+    player.on('timeupdate', throttle(onPlay, 1000));
+    function onPlay (data){
+         const currentTime = data.seconds;
+         localStorage.setItem(LOCALCURRENTTIME_KEY, currentTime);
+     };
+    setCurrentTime()
+    function setCurrentTime() {
+        if (!localStorage.getItem(LOCALCURRENTTIME_KEY)) {
+            return;
+        }
+        player.setCurrentTime(localStorage.getItem(LOCALCURRENTTIME_KEY));   
+    };
     
     /*
     Вивчи документацію методу on() і почни відстежувати подію timeupdate - 
-    оновлення часу відтворення.
+    оновлення часу відтворення.^^^^^^
 Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде
- рядок "videoplayer-current-time".
+ рядок "videoplayer-current-time".^^^^^^^^
 Під час перезавантаження сторінки скористайся методом setCurrentTime() з 
 метою відновлення відтворення зі збереженої позиції.
 Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+/*
+Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email
+ і message,у яких зберігай поточні значення полів форми. Нехай ключем для сховища 
+ буде рядок "feedback-form-state".^^^^^^
+Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, 
+заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.^~~~~~
+Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з 
+полями email, message та їхніми поточними значеннями.
+Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд.
+ Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
+*/
+import throttle from "lodash.throttle";
+
+const form = document.querySelector('.feedback-form');
+
+const STOREGE_KEY = 'feedback-form-state';
+form.addEventListener('submit', hendleFormSubmit);
+form.addEventListener('input', throttle(hendleFormInput, 500));
+
+populateInput()
+function hendleFormInput(ev) {
+     const formInform = {
+        email: form.email.value,
+        message: form.message.value,
+    }
+    const formInformJSON = JSON.stringify(formInform);
+    localStorage.setItem(STOREGE_KEY, formInformJSON);
+
+};
+ function populateInput() {
+        const savedValue = localStorage.getItem(STOREGE_KEY);
+        const parseSave = JSON.parse(savedValue);
+    if (savedValue) {
+        form.message.value = parseSave.message;
+        form.email.value = parseSave.email;
+    }
+};
+
+
+
+function hendleFormSubmit(ev) {
+    ev.preventDefault();
+
+    ev.currentTarget.reset();
+    localStorage.removeItem(STOREGE_KEY);
+    
+};
+
+
 
